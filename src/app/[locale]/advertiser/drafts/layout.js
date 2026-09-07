9c63147be08d1a259fd9ff89a9e01814a1b82838
Abood <abood.ali.t2055@gmail.com>
2026-09-07 21:47:51 +0300

    fix: remove duplicate Navbar, Sidebar, and DashboardShell from sub-layouts

--- src/app/[locale]/advertiser/drafts/layout.js
+++ src/app/[locale]/advertiser/drafts/layout.js
@@ -1,21 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
-import { getLocale } from "next-intl/server";
-import { AssistantProvider } from "@/context/AssistantContext";
-import { NotificationsProvider } from "@/context/NotificationsContext";
-import DashboardShell from "@/components/DashboardShell";
-
-export default async function DraftsLayout({ children }) {
-  const locale = await getLocale();
-  return (
-    <div dir={locale === "ar" ? "rtl" : "ltr"} className="min-h-screen">
-      <NotificationsProvider>
-        <AssistantProvider>
-          <Navbar />
-          <Sidebar />
-          <DashboardShell locale={locale}>{children}</DashboardShell>
-        </AssistantProvider>
-      </NotificationsProvider>
-    </div>
-  );
+export default function DraftsLayout({ children }) {
+  return children;
 }
